a9173aab6f2ecb3b3d7701a414aa107e726bb507
Vitaly Cheptsov <cheptsov@ispras.ru>
2020-11-11 18:23:30 +0300

    Lab9 added

--- kern/trap.c
+++ kern/trap.c
@@ -270,5 +270,36 @@ page_fault_handler(struct Trapframe *tf) {
 
   // LAB 8: Your code here.
 
+  // We've already handled kernel-mode exceptions, so if we get here,
+  // the page fault happened in user mode.
 
+  // Call the environment's page fault upcall, if one exists.  Set up a
+  // page fault stack frame on the user exception stack (below
+  // UXSTACKTOP), then branch to curenv->env_pgfault_upcall.
+  //
+  // The page fault upcall might cause another page fault, in which case
+  // we branch to the page fault upcall recursively, pushing another
+  // page fault stack frame on top of the user exception stack.
+  //
+  // The trap handler needs one word of scratch space at the top of the
+  // trap-time stack in order to return.  In the non-recursive case, we
+  // don't have to worry about this because the top of the regular user
+  // stack is free.  In the recursive case, this means we have to leave
+  // an extra word between the current top of the exception stack and
+  // the new stack frame because the exception stack _is_ the trap-time
+  // stack.
+  //
+  // If there's no page fault upcall, the environment didn't allocate a
+  // page for its exception stack or can't write to it, or the exception
+  // stack overflows, then destroy the environment that caused the fault.
+  // Note that the grade script assumes you will first check for the page
+  // fault upcall and print the "user fault va" message below if there is
+  // none.  The remaining three checks can be combined into a single test.
+  //
+  // Hints:
+  //   user_mem_assert() and env_run() are useful here.
+  //   To change what the user environment runs, modify 'curenv->env_tf'
+  //   (the 'tf' variable points at 'curenv->env_tf').
+
+  // LAB 9: Your code here.
 }
